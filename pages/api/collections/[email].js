f7412b5610c7326bda7740407ee99a306ad681fb
Prajwal S N <prajwalnadig21@gmail.com>
2023-03-04 23:50:12 +0530

    fix: bug in recursive dir nesting
    Signed-off-by: Prajwal S N <prajwalnadig21@gmail.com>

--- pages/api/collections/[email].js
+++ pages/api/collections/[email].js
@@ -15,16 +15,29 @@ export default async function handler(req, res) {
     let collTree = [];
     let collMap = collections.map((coll) => ({...coll, children: []}));
     collMap
-      .filter((coll) => (coll.parent == ''))
-      .forEach((coll) => { collTree.push(coll); });
-    collMap
-      .filter((coll) => (coll.parent != ''))
+      .filter((coll) => (coll?.parent == ''))
       .forEach((coll) => {
-        collTree
-          .find((item) => (item._id == coll?.parent))?
-          .children
-          .push(coll);
+        collTree.push(coll);
+        collMap.splice(collMap.indexOf(coll), 1);
       });
+
+    while (collMap.length !== 0) {
+      collMap
+        .forEach((coll, i) => {
+            findParent(collTree, coll.parent)?
+            .children
+            .push(coll);
+          collMap.splice(i, 1);
+       });
+    }
+
+    function findParent(arr, id) {
+      for (let item of arr) {
+        if (item._id == id) { return item; }
+        let p = findParent(item.children, id);
+        if (p) { return p; }
+      }
+    }
 
     res.status(200).json(collTree);
   } else if (req.method === 'DELETE') {

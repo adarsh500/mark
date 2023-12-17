36b04ca328b90e559c61c981ca57fabe1b8023f5
adarsh500 <adarshsulegai50@gmail.com>
2023-12-17 23:23:48 +0530

    chore: fix file name

--- app/signin/user-auth-form.tsx
+++ app/signin/user-auth-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Icons } from '@/components/icons';
+import { Icons } from '@/components/Icons';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';

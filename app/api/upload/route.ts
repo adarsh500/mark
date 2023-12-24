30fd0e39f377db57d49a222d067138836411a76e
Adarsh Sulegai <adarshsulegai50@gmail.com>
2023-12-24 23:25:15 +0530

    feat: add bookmark imports

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,45 @@
 import { writeFile, readFile } from 'fs/promises';
 import { NextRequest, NextResponse } from 'next/server';
+import cheerio from 'cheerio';
+
+const parseBookmarks = (html) => {
+  const $ = cheerio.load(html);
+  const bookmarks: any = [];
+
+  // Iterate through bookmark elements
+  $('a').each((_, element) => {
+    const title = $(element).text();
+    const url = $(element).attr('href');
+    const folder = $(element).parents('dl').prev('h3').text();
+
+    bookmarks.push({
+      title,
+      url,
+      folder,
+    });
+  });
+
+  return bookmarks;
+};
+
+const groupBookmarksByFolder = (bookmarks) => {
+  const groupedBookmarks = {};
+
+  bookmarks.forEach((bookmark) => {
+    const folder = bookmark.folder || 'Root';
+
+    if (!groupedBookmarks[folder]) {
+      groupedBookmarks[folder] = [];
+    }
+
+    groupedBookmarks[folder].push({
+      title: bookmark.title,
+      url: bookmark.url,
+    });
+  });
+
+  return groupedBookmarks;
+};
 
 export async function POST(request: NextRequest) {
   const data = await request.formData();
@@ -20,7 +60,12 @@ export async function POST(request: NextRequest) {
   console.log(`open ${path} to see the uploaded file`);
   //read the file and print contents out
   const contents = await readFile(path);
-  console.log(contents.toString());
+  const htmlContent = contents.toString();
+  console.log('htmlContent', htmlContent);
+  const bookmarks = parseBookmarks(htmlContent);
+  const groupedBookmarks = groupBookmarksByFolder(bookmarks);
+
+  console.log('\n\n', 'bk', bookmarks, '\n\n', groupedBookmarks);
 
   return NextResponse.json({ success: true });
 }
--- components/core/Bookmarks/BookmarksModal.tsx
+++ components/core/Bookmarks/BookmarksModal.tsx
@@ -153,15 +153,13 @@ const BookmarksModal = (props: any) => {
     try {
       const data = new FormData();
       data.set('file', file);
-
+      console.log('file', file);
       const res = await fetch('/api/upload', {
         method: 'POST',
         body: data,
       });
-      // handle the error
       if (!res.ok) throw new Error(await res.text());
     } catch (e: any) {
-      // Handle errors here
       console.error(e);
     }
   };
